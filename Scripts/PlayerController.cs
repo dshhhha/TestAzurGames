c3730dcb6ec108e9eda28cb5bb79f51804b4499a
dshhhha <134551921+dshhhha@users.noreply.github.com>
2023-11-22 17:46:13 +0300

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    public float forwardSpeed;
+    public float jumpForce;
+    public float Gravity = -20;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.z = forwardSpeed;
+
+        direction.y += Gravity * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
+    }
+    private void Jump()
+    {
+        direction.y = jumpForce;
+    }
+}
